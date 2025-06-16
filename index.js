4922d476e750f0ef108909538c5631639894cc96
izzy-Ti <israelashenafi29@gmail.com>
2025-06-16 09:14:27 -0700

    session handeling

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 import express from 'express'
 import router from './router.js'
 import cookieParser from 'cookie-parser'
+import session from 'express-session'
 
 const app = express()
 const PORT = 3000
 
 app.use(cookieParser())
-app.get('/', (req,res) =>{
-    res.cookie('name', 'Abebe')
-    res.send('Hello there')
+app.use(session({
+    secret:`sample-secret`,
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.get('/visit', (req,res) =>{
+    if(req.session.page_view){
+        req.session.page_view ++;
+        res.send(`You have visited this site ${req.session.page_view} times`)
+    } else {
+        req.session.page_view = 1;
+        res.send('Welcom to this page')
+    }
 })
-app.post('/fetch',express.json(), (req,res) =>{
-    const {name,email,password} = req.body
-    res.cookie('name',`${name}`)
-    res.cookie('email',`${email}`)
-    res.cookie('password',`${password}`)
-    res.send('Hello '+ `${name}` + ' ur logged in with ' + `${email}`)
+app.get('/', (req,res) =>{
+    res.send('Hello there')
 })
 app.listen(PORT,() =>[
     console.log(`server is running on http://localhost:${PORT}`)
